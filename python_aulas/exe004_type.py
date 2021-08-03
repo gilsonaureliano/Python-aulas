0aa0ebdb1a988ab0a72c80bb8d4863d011f78bd6
Gilson Aureliano <gilson.aureliano22@gmail.com>
2021-08-03 15:05:14 -0300

    Create exe004_type.py

---
+++ python_aulas/exe004_type.py
@@ -0,0 +1,7 @@
+q1 = input('Escreva qualquer coisa: ')
+print(type(q1))
+
+print('Foi uma letra digitada: ', q1.isalpha())
+print('Foi um numero digitado: ', q1.isnumeric())
+print('A palavra é alfanumerica: ', q1.isalnum())
+print('A palavra esta escreia em MAIUSCULO: ', q1.isupper())
